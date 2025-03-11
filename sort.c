8a3c7aab2bf4cffde82de2d228c105d912232f71
Док Браун <tanisimov2006@gmail.com>
2025-03-12 01:32:07 +0700

    изменен sort.c

--- sort.c
+++ sort.c
@@ -16,3 +16,21 @@ void selectionSort(int arr[], int n) {
         }
     }
 }
+
+int main() {
+    int a[] = {4, 2, 7, 3};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    printf("Исходный массив: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    selectionSort(a, n);
+    printf("Отсортированный массив: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
+}
